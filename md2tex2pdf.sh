02a239ef15a251833105152e7ca01fe5f012f0bc
Zeriuno <d.pitrolo@gmx.com>
2016-11-06 20:58:50 +0100

    Ignorer MIMOire-texte.pdf

--- md2tex2pdf.sh
+++ md2tex2pdf.sh
@@ -1,3 +1,5 @@
 #! /usr/bin/env bash
 
-pandoc "$@" -f markdown -t latex --latex-engine=xelatex  -s -o "$@".pdf
+filename="$@"
+basename="${filename%.*}"
+pandoc "$@" -f markdown -t latex --latex-engine=xelatex  -s -o ../"$basename".pdf
